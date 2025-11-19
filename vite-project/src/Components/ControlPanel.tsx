3926d073056f919a8761b4ac2f9485f30c2fc8c3
i-ganza007 <i.ganza@alustudent.com>
2025-11-19 12:33:36 +0200

    fix: every object created has random poss and click is working

--- vite-project/src/Components/ControlPanel.tsx
+++ vite-project/src/Components/ControlPanel.tsx
@@ -8,23 +8,27 @@ import {
 } from "@/Components/ui/toggle-group"
 
 export function ControlPanel() {
+    const [rect_x , rect_y,rect_z]:Number[] = [Math.random()*2,Math.random(),Math.random()]
+    const [box_x , box_y,box_z]:Number[] = [Math.random()*2,Math.random(),Math.random()]
     // const meshObjectStore = useMeshStore((state)=>state.meshObjectStore)
     const addMeshObject = useMeshStore(state=>state.addMeshObject)
     const handleCreateBox = () => {
-        addMeshObject(<Box/>)
+        addMeshObject({type:"box",pos:[box_x,box_y,box_z]})
+        console.log(useMeshStore?.getState().meshObjectStore)
         return null
     }
     const handleCreateRectangle = () => {
-        addMeshObject(<Rectangle/>)
+        addMeshObject({type:"rectangle",pos:[rect_x,rect_y,rect_z]})
+        console.log(useMeshStore.getState().meshObjectStore)
         return null
     }
   return (
     <ToggleGroup type="multiple" variant="outline" className="bg-gray-400" size="lg">
-      <ToggleGroupItem value="bold" aria-label="Toggle bold">
-        <Square className="h-9 w-9" onClick={()=>handleCreateBox()} />
+      <ToggleGroupItem value="bold" onClick={()=>handleCreateBox()} aria-label="Toggle bold">
+        <Square className="h-9 w-9" />
       </ToggleGroupItem>
-      <ToggleGroupItem value="italic" aria-label="Toggle italic">
-        <RectangleHorizontal className="h-4 w-4" onClick={()=>handleCreateRectangle()} />
+      <ToggleGroupItem value="italic" aria-label="Toggle italic" onClick={()=>handleCreateRectangle()}>
+        <RectangleHorizontal className="h-4 w-4" />
       </ToggleGroupItem>
       <ToggleGroupItem value="strikethrough" aria-label="Toggle strikethrough">
         <Underline className="h-4 w-4" />
