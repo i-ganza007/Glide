753b5aac2cf1fab1e8592908505713c8a720d5e0
i-ganza007 <i.ganza@alustudent.com>
2025-11-21 15:18:37 +0200

    feat: added the grid and canvas, for visualisation

--- vite-project/src/meshStore/meshStore.tsx
+++ vite-project/src/meshStore/meshStore.tsx
@@ -6,6 +6,6 @@ export const useMeshStore = create((set)=>({
     meshObjectStore : [],
     activeWires:false,
     addMeshObject:  (mesh)=>set((state)=>({meshObjectStore:[mesh,...state.meshObjectStore]})),
-    changeActiveWires : ()=>set(state=>({activeWires:!state.activeWires}))
-
+    changeActiveWires : ()=>set(state=>({activeWires:!state.activeWires})),
+    changeVisibleStatus: (id,newInfo) => set(state=>({}))
 }))
